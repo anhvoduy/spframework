ff91716f4eb11c9ea50a9c390ca75be02181df44
duyanh <duyanh2005@gmail.com>
2017-05-29 01:32:51 +0700

    - step 02: generate webpart + init environment

--- src/first-webpart/src/webparts/firstWebPart/FirstWebPartWebPart.ts
+++ src/first-webpart/src/webparts/firstWebPart/FirstWebPartWebPart.ts
@@ -2,7 +2,10 @@ import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneCheckbox,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
 } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
 
@@ -18,9 +21,22 @@ export default class FirstWebPartWebPart extends BaseClientSideWebPart<IFirstWeb
         <div class="${styles.container}">
           <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
             <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
-              <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-              <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
-              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
+              <span class="ms-font-xl ms-fontColor-white">
+                Welcome to SharePoint Framework!
+              </span>
+                     
+              <p class="ms-font-l ms-fontColor-white">
+                Customize SharePoint experiences using Web Parts.
+              </p>              
+              <p class="ms-font-l ms-fontColor-white">
+                ${escape(this.properties.description)}
+              </p>
+              <p class="ms-font-l ms-fontColor-white">
+                ${escape(this.properties.test2)}
+              </p>
+              <p class="ms-font-l ms-fontColor-white">Loading from 
+                ${escape(this.context.pageContext.web.title)}
+              </p>              
               <a href="https://aka.ms/spfx" class="${styles.button}">
                 <span class="${styles.label}">Learn more</span>
               </a>
@@ -46,7 +62,28 @@ export default class FirstWebPartWebPart extends BaseClientSideWebPart<IFirstWeb
               groupName: strings.BasicGroupName,
               groupFields: [
                 PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                  //label: strings.DescriptionFieldLabel
+                  label: 'Description'
+                }),
+                PropertyPaneTextField('test',{
+                  label: 'Multi-line Text Field',
+                  multiline: true
+                }),
+                PropertyPaneCheckbox('test1',{
+                  text: 'Checkbox'
+                }),
+                PropertyPaneDropdown('test2',{
+                  label: 'Dropdown',
+                  options: [
+                    {key: 1, text: 'one'},
+                    {key: 2, text: 'two'},
+                    {key: 3, text: 'three'}
+                  ]
+                }),
+                PropertyPaneToggle('test3',{
+                    label: 'Toogle',
+                    onText: 'On',
+                    offText: 'Off'
                 })
               ]
             }
--- src/first-webpart/src/webparts/firstWebPart/IFirstWebPartWebPartProps.ts
+++ src/first-webpart/src/webparts/firstWebPart/IFirstWebPartWebPartProps.ts
@@ -1,3 +1,7 @@
 export interface IFirstWebPartWebPartProps {
   description: string;
+  test: string;
+  test1: boolean;
+  test2: string;
+  test3: boolean;
 }
