7a82172ff21f1c7c814b16114719a767c3e8dda8
duyanh <duyanh2005@gmail.com>
2017-06-02 23:15:28 +0700

    remove redundant code

--- src/documentcard-webpart/src/webparts/documentCard/IDocumentCardWebPartProps.ts
+++
@@ -1,3 +0,0 @@
-export interface IDocumentCardWebPartProps {
-  description: string;
-}
--- src/documentcard-webpart/src/webparts/documentCard/tests/DocumentCard.test.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="mocha" />
-
-import { assert } from 'chai';
-
-describe('DocumentCardWebPart', () => {
-  it('should do something', () => {
-    assert.ok(true);
-  });
-});
