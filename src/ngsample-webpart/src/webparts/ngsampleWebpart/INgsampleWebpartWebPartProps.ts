64ac76107a416fe7e9e383031975ba08fd1d0137
duyanh <duyanh2005@gmail.com>
2017-06-03 15:31:14 +0700

    angular sample webPart

---
+++ src/ngsample-webpart/src/webparts/ngsampleWebpart/INgsampleWebpartWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface INgsampleWebpartWebPartProps {
+  description: string;
+}
---
+++ src/ngsample-webpart/src/webparts/ngsampleWebpart/tests/NgsampleWebpart.test.ts
@@ -0,0 +1,9 @@
+/// <reference types="mocha" />
+
+import { assert } from 'chai';
+
+describe('NgsampleWebpartWebPart', () => {
+  it('should do something', () => {
+    assert.ok(true);
+  });
+});
