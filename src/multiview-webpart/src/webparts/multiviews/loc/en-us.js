a6c8a1e8ae22083d4f0d29b8e9dd912bbbe0631b
devinsg <duyanh2005@gmail.com>
2019-07-20 16:44:48 +0700

    up

--- src/multiview-webpart/src/webparts/multiviews/loc/en-us.js
+++ src/multiview-webpart/src/webparts/multiviews/loc/en-us.js
@@ -1,9 +1,9 @@
 define([], function() {
   return {
     "PropertyPaneDescription": "MultiViews Webpart",
-    "BasicGroupName": "Group Name",
+    "BasicGroupName": "Basic Group Name",
     "DescriptionFieldLabel": "Description",
-    
+
     "DataGroupName": "Data Group Name",
     "ListNameFieldLabel": "List Name",
     "PollTitleFieldLabel": "Poll Title",
