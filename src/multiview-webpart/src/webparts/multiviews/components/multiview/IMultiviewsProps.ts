f79d6c926cce2b57097973fe9a36af45c67b89e3
devinsg <duyanh2005@gmail.com>
2019-07-20 22:44:57 +0700

    clean up sample view

--- src/multiview-webpart/src/webparts/multiviews/components/multiview/IMultiviewsProps.ts
+++
@@ -1,6 +0,0 @@
-export interface IMultiviewsProps {
-  description: string;
-  listName: string;
-  pollTitle: string;
-  pollDescription: string;
-}
--- src/multiview-webpart/src/webparts/multiviews/components/multiview/welcome/IWelcomeProps.ts
+++
@@ -1,6 +0,0 @@
-export interface IWelcomeProps {
-  title: string;
-  listName: string;
-  pollTitle: string;
-  pollDescription: string;
-}
