962f2cf015dbd768c5df74bf0a71b1226508d13d
anhvoduy <voduyanh1984@gmail.com>
2019-07-20 12:22:43 +0700

    init app

---
+++ src/multiview-webpart/src/webparts/multiviews/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMultiviewsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MultiviewsWebPartStrings' {
+  const strings: IMultiviewsWebPartStrings;
+  export = strings;
+}
