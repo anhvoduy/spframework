d013d4b6202d6722e995bebecea3b2889bc91d73
devinsg <duyanh2005@gmail.com>
2019-07-20 16:42:53 +0700

    display web part properties (con)

--- src/multiview-webpart/src/webparts/multiviews/MultiviewsWebPart.ts
+++ src/multiview-webpart/src/webparts/multiviews/MultiviewsWebPart.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import { Version, Environment, EnvironmentType } from '@microsoft/sp-core-library';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import {
   IPropertyPaneConfiguration,
@@ -8,20 +8,34 @@ import {
 } from '@microsoft/sp-property-pane';
 
 import * as strings from 'MultiviewsWebPartStrings';
+import { IMultiviewsWebPartProps } from './IMultiviewsWebPartProps';
 import Multiviews from './components/Multiviews';
 import { IMultiviewsProps } from './components/IMultiviewsProps';
 
-export interface IMultiviewsWebPartProps {
-  description: string;
-}
+import { IPollService, PollService, MockPollService } from './services/index';
 
 export default class MultiviewsWebPart extends BaseClientSideWebPart<IMultiviewsWebPartProps> {
+  private pollService: IPollService;
+
+  protected onInit(): Promise<void> {
+    if (DEBUG && Environment.type === EnvironmentType.Local) {
+      this.pollService = new MockPollService();
+    } else {
+      this.pollService = new PollService(this.context);
+    }
+
+    this.configureWebPart = this.configureWebPart.bind(this);
+    return super.onInit();
+  }
 
   public render(): void {
     const element: React.ReactElement<IMultiviewsProps > = React.createElement(
       Multiviews,
       {
-        description: this.properties.description
+        description: this.properties.description,
+        listName: this.properties.listName,
+        pollTitle: this.properties.pollTitle,
+        pollDescription: this.properties.pollDescription,
       }
     );
 
@@ -51,10 +65,28 @@ export default class MultiviewsWebPart extends BaseClientSideWebPart<IMultiviews
                   label: strings.DescriptionFieldLabel
                 })
               ]
+            },
+            {
+              groupName: strings.DataGroupName,
+              groupFields: [
+                PropertyPaneTextField('listName', {
+                  label: strings.ListNameFieldLabel
+                }),
+                PropertyPaneTextField('pollTitle', {
+                  label: strings.PollTitleFieldLabel
+                }),
+                PropertyPaneTextField('pollDescription', {
+                  label: strings.PollDescriptionFieldLabel
+                })
+              ]
             }
           ]
         }
       ]
     };
+  }
+
+  private configureWebPart(): void {
+    this.context.propertyPane.open();
   }
 }
--- src/multiview-webpart/src/webparts/multiviews/components/Multiviews.tsx
+++ src/multiview-webpart/src/webparts/multiviews/components/Multiviews.tsx
@@ -7,7 +7,10 @@ export default class Multiviews extends React.Component<IMultiviewsProps, {}> {
   public render(): React.ReactElement<IMultiviewsProps> {
     return (
       <div className={ styles.multiviews }>
-        <Welcome title={this.props.description} />        
+        <Welcome  title={this.props.description}
+                  listName={this.props.listName}
+                  pollTitle={this.props.pollTitle}
+                  pollDescription={this.props.pollDescription} />        
       </div>
     );
   }
